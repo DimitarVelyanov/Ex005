08b05f4368e191ef4650ec3990462ccd655f3161
d_velqnov <d_velqnov@abv.bg>
2016-02-17 21:00:51 +0200

    .

---
+++ Maine1.java
@@ -0,0 +1,44 @@
+
+public class Maine1 {
+
+	public static void main(String[] args) {
+		boolean result = true;
+		System.out.println(result);
+
+		int numberInDec = 16;
+		int numberInOct = 020;
+		int numberInNex = 0x10;
+		System.out.println(numberInDec);
+		System.out.println(numberInOct);
+		System.out.println(numberInNex);
+
+		long longInt = 2344L;
+		System.out.println(longInt);
+		
+		float realNumber=12.5f;
+		System.out.println(realNumber);
+		
+		realNumber=1.25e+2f;
+		System.out.println(realNumber);
+		 
+		double d =12.5d;
+		System.out.println(d);
+		
+		double dd=12.5;
+		System.out.println(dd);
+		
+		char simbolOne='a';
+		System.out.println(simbolOne);
+		
+		char simbolTwo='\u0061';
+		System.out.println(simbolTwo);
+		
+		String str="Cat car\n\n\n";
+		System.out.print(str);
+		
+		
+		
+
+	}
+
+}
